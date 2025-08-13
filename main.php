ac0eb047975fc10fb42af5e382773fcfab76c5b3
Jerome-Seroma <darkenedhydra02@gmail.com>
2025-08-13 02:30:37 +0000

    UnderstandingTheFunction

---
+++ main.php
@@ -0,0 +1,24 @@
+<?php
+function calculate( $a, $b, $command): mixed {
+    switch($command) {
+        case 'add':
+            return $a + $b;
+        case 'subtract':
+            return $a - $b;
+        case 'multiply':
+            return $a * $b;
+        case 'division':
+            return $a / $b;
+        default:
+            return "invalid operation";
+    }    
+}
+
+echo calculate(a: 10, b: 5, command: 'add');
+echo "\n";
+echo calculate(a: 20, b: 10, command: 'subtract');
+echo "\n";
+echo calculate(a: 5, b: 2, command: 'multiply');
+echo "\n";
+echo calculate(a: 7, b: 2, command: 'division');
+echo "\n";
